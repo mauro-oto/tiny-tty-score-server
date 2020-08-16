ebcec7ad140dee37d2ddaec5234a128f036566b5
Mauro Otonelli <mauro.otonelli@gmail.com>
2020-08-16 00:46:07 -0300

    fix syntax error

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -8,7 +8,7 @@ class ScoresController < ApplicationController
 
   def save_score
     Rails.logger.info request.user_agent
-    if request.user_agent && request.user_agent.start_with? "DragonRuby"
+    if request.user_agent && request.user_agent.start_with?("DragonRuby")
       Score.create(name: params[:name], score: params[:score])
       head :ok
     else
